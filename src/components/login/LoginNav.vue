07cfbe7286cfad65c22ab50773f0416c08110d4b
Gihyun-developSpace <glenkoh@naver.com>
2021-09-27 13:51:31 +0900

    로그인추가

--- src/components/login/LoginNav.vue
+++ src/components/login/LoginNav.vue
@@ -9,6 +9,28 @@
 			</li>
 		</ul>
 	</nav>
+	<div class="idbox">
+		<Logo />
+		<p>로그인이 필요한 서비스 입니다.</p>
+		<label for="formGroupExampleInput" class="form-label"></label>
+		<input
+			type="text"
+			class="form-control"
+			id="formGroupExampleInput"
+			placeholder="이메일을 입력해주세요!"
+		/>
+	</div>
+	<div class="passwordbox">
+		<label for="formGroupExampleInput2" class="form-label"></label>
+		<input
+			type="text"
+			class="form-control"
+			id="formGroupExampleInput2"
+			placeholder="비밀번호를 입력해주세요!"
+		/>
+	</div>
+	<button type="button" class="btn btn-primary btn-lg">로그인</button>
+	<button type="button" class="btn btn-primary btn-lg">회원가입</button>
 </template>
 
 <script>
@@ -42,6 +64,7 @@ nav {
 	justify-content: space-between;
 	align-items: center;
 	padding: 20px 40px;
+
 	ul {
 		margin: 0;
 		display: flex;
@@ -50,6 +73,15 @@ nav {
 			@include rem(20);
 			text-decoration: none;
 		}
+	}
+}
+.idbox {
+	width: 30%;
+	margin: 0 auto;
+	p {
+		text-align: center;
+		font-size: 25px;
+		color: $primary;
 	}
 }
 </style>
