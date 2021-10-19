daa299e1e7e24dd9b24b41e746bf5c5f59ed09e4
Gihyun-developSpace <glenkoh@naver.com>
2021-10-19 22:17:44 +0900

    모달창 만들기

--- src/components/home/Modal.vue
+++ src/components/home/Modal.vue
@@ -9,7 +9,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+					<h5 class="modal-title" id="exampleModalLabel">게시물 작성하기</h5>
 					<button
 						type="button"
 						class="btn-close"
@@ -17,16 +17,27 @@
 						aria-label="Close"
 					></button>
 				</div>
-				<div class="modal-body">...</div>
+
+				<div class="modal-body">
+					<div>
+						<label for="exampleFormControlTextarea1" class="form-label"></label>
+						<textarea
+							class="form-control"
+							id="exampleFormControlTextarea1"
+							placeholder="제봉님!오늘 무슨일이 있었나요?"
+							rows="5"
+						></textarea>
+					</div>
+					<i class="bi bi-image"></i>
+				</div>
 				<div class="modal-footer">
 					<button
 						type="button"
-						class="btn btn-secondary"
+						class="btn btn-primary subbtn"
 						data-bs-dismiss="modal"
 					>
-						Close
+						게시글 게시
 					</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
 				</div>
 			</div>
 		</div>
@@ -37,4 +48,21 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.subbtn {
+	width: 100%;
+	border-radius: 10px;
+	height: 50px;
+}
+.modal-body {
+	position: relative;
+}
+
+.bi-image {
+	color: $primary;
+	position: absolute;
+	top: 140px;
+	bottom: 0;
+	right: 30px;
+}
+</style>
