9fd7327fe0d8d29aae89ac68b90046105b65786e
Chan Gyu <80776262+changyuyeo@users.noreply.github.com>
2021-10-30 19:02:13 +0900

    프로필 작업 준비

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,11 +1,7 @@
 import { auth } from './index'
 
-function signupUser(payload) {
-	return auth.post(`sign/up`, payload)
-}
+const signupUser = payload => auth.post(`sign/up`, payload)
 
-function loginUser(payload) {
-	return auth.post(`sign`, payload)
-}
+const loginUser = payload => auth.post(`sign`, payload)
 
 export { signupUser, loginUser }
