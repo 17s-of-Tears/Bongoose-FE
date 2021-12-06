b6de571c22ace902b2de5621ae0f6bffbd06e51f
Gihyun-developSpace <glenkoh@naver.com>
2021-12-06 19:45:07 +0900

    '오른쪽친구창데이터연결'

--- src/api/user.js
+++ src/api/user.js
@@ -7,4 +7,9 @@ const updateUser = payload => user.put(`me`, payload)
 //유저 정보 가져오기
 const getUsers = payload => user.get('/', payload)
 
-export { userInfo, updateUser, getUsers }
+const getRandomFriends = payload => user.get('random', payload)
+
+//다른 유저 프로필 자세히보기
+const userDetail = userId => user.get(`/${userId}`)
+
+export { userInfo, updateUser, getUsers, getRandomFriends, userDetail }
