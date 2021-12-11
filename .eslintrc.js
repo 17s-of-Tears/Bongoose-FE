b27814fa6bbd8fd9980a1fc53655ea52e42e2b44
Chan Gyu <wry5533@gmail.com>
2021-12-11 16:41:23 +0900

    eslint 재설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,19 @@ module.exports = {
 	env: {
 		node: true
 	},
-	extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
-	parserOptions: {
-		parser: '@typescript-eslint/parser'
-	},
-	rules: {
-		'prettier/prettier': [
-			'error',
-			{
-				arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
-				bracketSpacing: true, // 객체 리터럴에서 괄호에 공백 삽입 여부
-				endOfLine: 'auto', // EoF 방식, OS별로 처리 방식이 다름
-				semi: false, // 세미콜론 사용 여부
-				singleQuote: true, // single 쿼테이션 사용 여부
-				tabWidth: 2, // 탭 너비
-				trailingComma: 'none', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
-				printWidth: 80, // 텍스트 길이 지정
-				useTabs: true // 탭 사용 여부
-			}
-		]
-	},
 	extends: [
 		'plugin:vue/vue3-essential',
 		'eslint:recommended',
+		'@vue/typescript/recommended',
 		'@vue/prettier',
-		'@vue/typescript'
-	]
+		'@vue/prettier/@typescript-eslint',
+		'prettier'
+	],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+	}
 }
